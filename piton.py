89a15233e10ecf2960d8a1cf0fa490739195c5c9
buribu <buribu@gmail.com>
2016-04-04 02:24:44 -0300

    arquivo test nvim

---
+++ piton.py
@@ -0,0 +1,32 @@
+import math 
+from math import *
+
+
+class bugabi:
+    def __int__():
+        a = 5
+	b = 6
+
+if __name__ == __init__:
+     print("sorry guys")   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
