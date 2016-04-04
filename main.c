2fa30304af8cd52618a82caaed1a1819ee256466
buribu <buribu@gmail.com>
2016-04-04 01:37:39 -0300

    clang on android \o/

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
-#include "minhalib.h"
-int main(int argc, char *argv[]){
-    int x,y,z;
-    
-    z = 2+3;
-   
-	printf("%d",z);
-	
+
+int main( void ){
+	printf("ola mundo\n");
 	return 0;
 }
